9144202679b0d2e63c942d767e9c1bb554649d12
57518_Kunal <kunalgosavi4298@gmail.com>
2022-09-10 11:30:52 +0530

    Updated By Kunal

---
+++ review3/DocManagment/src/main/java/com/DocumentManagment/DocManagmentApplication.java
@@ -0,0 +1,14 @@
+package com.DocumentManagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocManagmentApplication.class, args);
+		
+	}
+
+}
